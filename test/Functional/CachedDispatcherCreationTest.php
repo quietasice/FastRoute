6e09b3ee6baa67110959cd8e4e7e308bbc3c7cd9
Luís Cobucci <lcobucci@gmail.com>
2019-12-18 16:24:36 +0100

    Add functional test to cover cached dispatchers
    Allowing us to more reliably modify things in the lib.

---
+++ test/Functional/CachedDispatcherCreationTest.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+namespace FastRoute\Test\Functional;
+
+use FastRoute\RouteCollector;
+use PHPUnit\Framework\TestCase;
+use function FastRoute\cachedDispatcher;
+use function file_exists;
+use function unlink;
+
+final class CachedDispatcherCreationTest extends TestCase
+{
+    private const OPTIONS = ['cacheFile' => __DIR__ . '/router.php'];
+
+    /** @after */
+    public function removeCacheFile(): void
+    {
+        if (! file_exists(self::OPTIONS['cacheFile'])) {
+            return;
+        }
+
+        unlink(self::OPTIONS['cacheFile']);
+    }
+
+    /** @test */
+    public function routeLoaderShouldAlwaysBeCalledWhenCacheIsDisabled(): void
+    {
+        $calls   = 0;
+        $loader  = $this->createSimpleLoader($calls);
+        $options = self::OPTIONS + ['cacheDisabled' => true];
+
+        cachedDispatcher($loader, $options);
+        cachedDispatcher($loader, $options);
+        cachedDispatcher($loader, $options);
+
+        self::assertSame(3, $calls);
+    }
+
+    /** @test */
+    public function routeLoaderShouldBeCalledOnlyOnceWhenUsingCache(): void
+    {
+        $calls  = 0;
+        $loader = $this->createSimpleLoader($calls);
+
+        cachedDispatcher($loader, self::OPTIONS);
+        cachedDispatcher($loader, self::OPTIONS);
+        cachedDispatcher($loader, self::OPTIONS);
+
+        self::assertSame(1, $calls);
+    }
+
+    private function createSimpleLoader(int &$calls): callable
+    {
+        return static function (RouteCollector $routes) use (&$calls): void {
+            ++$calls;
+
+            $routes->addRoute('GET', '/', 'testing');
+        };
+    }
+}
